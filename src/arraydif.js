989235ab5fc5ac795b4a8086a112d9e45da41980
Greg Hurrell <victor.0w3@hotmail.com>
2016-08-06 15:48:04 -0400

    examples

---
+++ src/arraydif.js
@@ -0,0 +1,4 @@
+//It should remove all values from list a, which are present in list b.
+//difference([1,2],[1]) == [2] difference([1,2,2,2,3],[2]) == [1,3]
+let array_diff = (a, b) => a.reduce((prev, curr) => (!b.includes(curr) && prev.push(curr), prev), [])
+console.log(array_diff([1,2,2,2,3], [2]));
---
+++ src/multiple-below.js
@@ -0,0 +1,10 @@
+//Returns the sum of all the multiples of 3 or 5 below the number passed in.
+function multiplebelow(number){
+	return number <= 0 ? 0 
+		:Array.from(new Array(number-1), (x, i) => i + 1)
+		.reduce((prev, curr) => (curr % 3 == 0 || curr % 5 == 0) ? prev + curr: prev + 0 ,0)
+}
+
+console.log(multiplebelow(10))//23
+console.log(multiplebelow(5))//3
+console.log(multiplebelow(2))//0
